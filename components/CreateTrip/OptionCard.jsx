85cf3ba37a9ef08b24128b9ed7feb6eb09e17bbb
codelab265 <mphatsomlenga1@gmail.com>
2024-09-28 07:10:33 +0200

    font changes

--- components/CreateTrip/OptionCard.jsx
+++ components/CreateTrip/OptionCard.jsx
@@ -1,32 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Colors } from '../../constants/Colors'
+import { View, Text } from "react-native";
+import React from "react";
+import { Colors } from "../../constants/Colors";
 
-export default function OptionCard({option,selectedOption}) {
+export default function OptionCard({ option, selectedOption }) {
   return (
-    <View style={[{
-        padding:25,
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'space-between',
-        backgroundColor:Colors.LIGHT_GRAY,
-        borderRadius:15
-    },selectedOption?.id==option?.id&&{borderWidth:3}]}>
-        <View>
-                <Text style={{
-                    fontSize:20,
-                    fontFamily:'outfit-bold'
-                }}>{option?.title}</Text>
-                 <Text style={{
-                    fontSize:17,
-                    fontFamily:'outfit',
-                    color:Colors.GRAY
-                }}>{option?.desc}</Text>
-        </View>
-        <Text style={{
-            fontSize:35
-        }}>{option.icon}</Text>
-   
+    <View
+      style={[
+        {
+          padding: 25,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          backgroundColor: Colors.LIGHT_GRAY,
+          borderRadius: 15,
+        },
+        selectedOption?.id == option?.id && { borderWidth: 3 },
+      ]}
+    >
+      <View>
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "outfit-bold",
+          }}
+        >
+          {option?.title}
+        </Text>
+        <Text
+          style={{
+            fontSize: 17,
+            fontFamily: "outfit-medium",
+            color: Colors.GRAY,
+          }}
+        >
+          {option?.desc}
+        </Text>
+      </View>
+      <Text
+        style={{
+          fontSize: 35,
+        }}
+      >
+        {option.icon}
+      </Text>
     </View>
-  )
+  );
 }
--- components/TripDetails/HotelCard.jsx
+++ components/TripDetails/HotelCard.jsx
@@ -1,56 +1,75 @@
-import { View, Text, Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { GetPhotoRef } from '../../services/GooglePlaceApi';
+import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { GetPhotoRef } from "../../services/GooglePlaceApi";
 
-export default function HotelCard({item}) {
- 
-    const [photoRef,setPhotoRef]=useState();
-    useEffect(()=>{
-        GetGooglePhotoRef();
-    },[])
-    
-    const GetGooglePhotoRef=async()=>{
-        const result= await GetPhotoRef(item.hotelName);
-        setPhotoRef(result);
-       }
-    return (
-    <View style={{
-        marginRight:20,
-        width:180,
-    }}>
-        <Image source={{uri:
-        'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference='
-        +photoRef
-        +'&key='+process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY}}
+export default function HotelCard({ item }) {
+  const [photoRef, setPhotoRef] = useState();
+  useEffect(() => {
+    GetGooglePhotoRef();
+  }, []);
+
+  const GetGooglePhotoRef = async () => {
+    const result = await GetPhotoRef(item.hotelName);
+    setPhotoRef(result);
+  };
+  return (
+    <View
+      style={{
+        marginRight: 20,
+        width: 180,
+      }}
+    >
+      <Image
+        source={{
+          uri:
+            "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=" +
+            photoRef +
+            "&key=" +
+            process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY,
+        }}
+        style={{
+          width: 180,
+          height: 120,
+          borderRadius: 15,
+        }}
+      />
+      <View
+        style={{
+          padding: 5,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-medium",
+            fontSize: 17,
+          }}
+        >
+          {item.hotelName}
+        </Text>
+
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text
             style={{
-                width:180,
-                height:120,
-                borderRadius:15,
-
+              fontFamily: "outfit-medium",
             }}
-        />
-        <View style={{
-            padding:5
-        }}>
-            <Text style={{
-                fontFamily:'outfit-medium',
-                fontSize:17,
-                
-            }}>{item.hotelName}</Text>
-
-            <View style={{
-                display:'flex',
-                flexDirection:'row',
-                justifyContent:'space-between'
-            }}>
-                <Text style={{
-                    fontFamily:'outfit'
-                }}>⭐ {item.rating}</Text>
-                  <Text style={{
-                    fontFamily:'outfit'
-                }}>💰 {item.price}</Text>
-            </View>
+          >
+            ⭐ {item.rating}
+          </Text>
+          <Text
+            style={{
+              fontFamily: "outfit-medium",
+            }}
+          >
+            💰 {item.price}
+          </Text>
         </View>
+      </View>
     </View>
-  )
+  );
 }
